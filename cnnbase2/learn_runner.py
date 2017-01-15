6e57cd6ad1cb814fa2cf2a35e97f5ca609c1a520
Jacek Skoczylas <gjacek6@gmail.com>
2017-01-15 13:45:51 +0100

    seconf learn Model1

--- cnnbase2/learn_runner.py
+++ cnnbase2/learn_runner.py
@@ -4,7 +4,9 @@ from cnnbase2.models import Model1
 if __name__ == '__main__':
     model_filename = 'run1'
     config = CnnDirsConfig()
-    model = Model1(config, '1000examples', model_filename)
-    # model.load_from_file()
-    history = model.learn_now(50)
-    model.save_model_to_file()
+    model = Model1(config, '5000examples', model_filename)
+    model.load_from_file()
+    for i in range(2, 10):
+        history = model.learn_now(50)
+        model.save_model_to_file("%s-epoch%d" % (model_filename, i*50))
+        print "Saved: %s-epoch%d" % (model_filename, i*50)
