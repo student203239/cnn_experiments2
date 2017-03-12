0763828a1461e2b4b3217200302f5c244d7c6ca1
Jacek Skoczylas <gjacek6@gmail.com>
2017-03-12 18:31:19 +0100

    learn flic2

--- cnnbase2/learn_runner2.py
+++ cnnbase2/learn_runner2.py
@@ -1,9 +1,20 @@
+import os
+
+import time
+
 from cnnbase2.load_data import CnnDirsConfig
 from cnnbase2.models import Model1, Model2, Model3, Model4, Model5, Model6
 
 if __name__ == '__main__':
     config = CnnDirsConfig()
-    model_filename = 'flic1'
-    model2 = Model6(config, 'flic.valid.07', model_filename)
-    model2.learn_now(50)
+    model_filename = 'flic2'
+    model2 = Model5(config, 'flic.valid.07', model_filename)
+    model2.learn_now(120)
     model2.save_model_to_file()
+
+    model2 = Model6(config, 'flic.valid.07', model_filename)
+    model2.learn_now(120)
+    model2.save_model_to_file()
+
+    time.sleep(30)
+    os.system("shutdown.exe /f /h")
